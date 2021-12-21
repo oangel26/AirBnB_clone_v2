895de9807c65616e6c8ae20f13fb97aa9def884e
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-21 15:46:36 -0500

    initial version

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,8 +5,8 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
+    __engine = None
+    __session = None
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
