1aa81551593c2e4c818ebb94dcfe027f9f48aca4
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-21 14:34:38 -0500

    init fixed

--- models/base_model.py
+++ models/base_model.py
@@ -10,17 +10,15 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     
-    id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    state_id = Column(DateTime, default=datetime.datetime.utcnow)
+
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.id = Column(String(60), nullable=False, primary_key=True, default=uuid.uuid4())
+            self.created_at = Column(DateTime, default=datetime.datetime.utcnow)
+            self.state_id = Column(DateTime, default=datetime.datetime.utcnow)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
