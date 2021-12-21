e302d87230fa9309125b6b36242cdc8c8a832f74
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-20 19:31:49 -0500

    privileges granted

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- Create the database hbnb_dev_db in your MySQL server.
+-- If the database hbtn_0c_0 already exists, the script should not fail.
+-- Create a new user hbnb_dev (in localhost) and a password.
+-- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database).
+-- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database).
+
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
