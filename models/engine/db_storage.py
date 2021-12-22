af1acbcfbd276b376fd43c49795036a0c90273b4
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-21 19:39:42 -0500

    updated files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,7 @@
 import json
 from sqlalchemy import (create_engine)
 import os
-
+from sqlalchemy.orm import sessionmaker
 
 class DBStorage:
     """This class manages storage of hbnb models in database format"""
@@ -15,19 +15,20 @@ class DBStorage:
 		self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
         os.getenv('HBNB_MYSQL_USER'), os.getenv('HBNB_MYSQL_PWD'), os.getenv('HBNB_MYSQL_HOST'), os.getenv('HBNB_MYSQL_DB'), pool_pre_ping=True)
 		Base.metadata.create_all(DBStorage.__engine)
+		Session = sessionmaker(bind=engine)
+        self.__session = Session()
 
 		if os.getenv('HBNB_ENV') == 'test':
 			self.__table__.drop(DBStorage.__engine)
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        new_dict = {}
-        if cls is not None:
-            for k, v in DBStorage.__session.items():
-                if k.split('.')[0] == cls.__name__:
-                    new_dict[k] = v
-            return new_dict
-        return DBStorage.__session
+
+        if cls is None:
+			new_dict = {}
+			for q in self.__session.query().all():
+                k = '{}.{}'.format(q.__class__.__name__, q.id)
+				new_dict[k] = q
 
     def new(self, obj):
         """Add the object to the current database session"""
--- models/state.py
+++ models/state.py
@@ -2,13 +2,28 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
+from os import getenv
+from models import City
+from models.__init__ import storage
 
 class State(BaseModel, Base):
-    """ State class """
-    
-    name = ""
-    
+    """ State class """    
     __tablename__ = "states"
 
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+
+    @property
+    def cities(self):
+        """ FileStorage relationship between cities and state"""
+        city_list = []
+        for v in storage.__objects.values():
+            if "state_id" in v.keys():
+                if v["state_id"] == self.id:
+                    city_list.append(v)
+        return city_list
