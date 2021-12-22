cadbfc8bfb98f46c20f30fc19eb40878a1c597f5
oangel26 <oa.angel26@gmail.com>
2021-12-22 18:01:18 -0500

    upload

--- models/city.py
+++ models/city.py
@@ -15,6 +15,7 @@ class City(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", back_populates="cities", cascade="all, delete-orphan")
     else:
         state_id = ""
         name = ""
--- models/user.py
+++ models/user.py
@@ -2,8 +2,9 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from os import getenv
-
+import models
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -14,6 +15,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", back_populates="user",
+                              cascade="all, delete-orphan")
     else:
         email = ''
         password = ''
