996d673fa8b3a93c9e7248085bfdeb13499911d0
jcgonzalezb <jcgonzalez@gmail.com>
2021-12-21 21:46:24 -0500

    pep8 fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,44 +13,45 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-		"""Returns a dictionary of models currently in storage"""
-		self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-        os.getenv('HBNB_MYSQL_USER'), os.getenv('HBNB_MYSQL_PWD'), os.getenv('HBNB_MYSQL_HOST'), os.getenv('HBNB_MYSQL_DB'), pool_pre_ping=True)
-		Base.metadata.create_all(DBStorage.__engine)
-		Session=sessionmaker(bind=engine)
-        self.__session=Session()
+        """Returns a dictionary of models currently in storage"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            os.getenv('HBNB_MYSQL_USER'), os.getenv('HBNB_MYSQL_PWD'), os.getenv('HBNB_MYSQL_HOST'), os.getenv('HBNB_MYSQL_DB'), pool_pre_ping=True))
+        Base.metadata.create_all(DBStorage.__engine)
+        Session = sessionmaker(bind=engine)
+        self.__session = Session()
 
-		if os.getenv('HBNB_ENV') == 'test':
-			self.__table__.drop(DBStorage.__engine)
+        if os.getenv('HBNB_ENV') == 'test':
+            self.__table__.drop(DBStorage.__engine)
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
 
         if cls is None:
-			new_dict={}
-			for q in self.__session.query().all():
-                k='{}.{}'.format(q.__class__.__name__, q.id)
-				new_dict[k]=q
-			return new_dict
+            new_dict = {}
+            for q in self.__session.query().all():
+                k = '{}.{}'.format(q.__class__.__name__, q.id)
+                new_dict[k] = q
+            return new_dict
 
+        new_dict = {}
         for q in self.__session.query(cls).all():
-                k='{}.{}'.format(q.__class__.__name__, q.id)
-				new_dict[k]=q
-			return new_dict
+            k = '{}.{}'.format(q.__class__.__name__, q.id)
+            new_dict[k] = q
+        return new_dict
 
     def new(self, obj):
         """Add the object to the current database session"""
-		if obj is not None:
+        if obj is not None:
             self.__session.add(obj)
 
     def save(self):
         """Commit all changes of the current database session"""
-		self.__session.commit()
+        self.__session.commit()
 
     def delete(self, obj=None):
         """Delete from the current database session"""
         if obj is not None:
-			self.__session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
         """Loads storage dictionary from database"""
@@ -62,9 +63,9 @@ class DBStorage:
         from models.amenity import Amenity
         from models.review import Review
 
-		Base.metadata.create_all(DBStorage.__engine)
+        Base.metadata.create_all(DBStorage.__engine)
 
-        session_factory=sessionmaker(
+        session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
-        Session=scoped_session(session_factory)
-		self.__session=Session()
+        Session = scoped_session(session_factory)
+        self.__session = Session()
