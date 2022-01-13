d0c566d698f9f38d7a562ec83cdd70c251e25611
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-13 15:59:29 -0500

    sed path fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,10 @@ apt-get update -y
 apt-get install nginx -y
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
 sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex off;\n}' /etc/nginx/sites-available/default
 service nginx restart
