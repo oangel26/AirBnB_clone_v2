c91422091412ecec1e32bf3aa0596f430ac3ec14
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-13 16:34:21 -0500

    initial version

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,15 @@
 Fabric script that generates a .tgz archive from the contents
 of the web_static folder. Use the function do_pack.
 """
+from fabric.api import local
+from datetime import datetime
 
 def do_pack():
-	run("NOWT=$(date +"%F %T")")
-	run("/versions/web_static_$NOW.tar.gz /web_static")
+	local("mkdir -p /versions/")
+	day =datetime.now().strftime("%Y%m%d%H%m%s")
+	compr_file =  "/versions/web_static_{}".format(day)
+	local("tar -czf {}.tgz /web_static".format(compr_file))
+	try:
+		return compr_file
+	except: 
+		return None
