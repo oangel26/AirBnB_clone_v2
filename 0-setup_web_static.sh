d5883dc2ad4ccab147324d7bea09f934ec1dada9
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-12 20:54:47 -0500

    initial version

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,11 +4,11 @@
 apt-get update -y
 apt-get install nginx -y
 service nginx restart
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 touch /data/web_static/releases/test/index.html
-echo "Hello World!" > /data/web_static/releases/test/index.html
-
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown ubuntu: data
+sed -i "/server_name _/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+nginx -s reload
+service nginx restart
