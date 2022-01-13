067e85a66bac573a79a03a9b81644ed71085195f
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-13 17:34:21 -0500

    syntax fixed in mkdir

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     Function to create a .tgz archive.
     """
     try:
-        local("mkdir -p /versions/")
+        local("mkdir -p versions")
         d_t = datetime.now().strftime("%Y%m%d%H%M%S")
         compr_file = "versions/web_static_{}.tgz".format(d_t)
         local("tar -cvzf {} web_static".format(compr_file))
