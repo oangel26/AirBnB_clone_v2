9e41529f0503e3a0b4d53b731bc9f72de9791496
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-13 15:47:37 -0500

    file fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,10 +3,10 @@
 
 apt-get update -y
 apt-get install nginx -y
+mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "/server_name _/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\tautoindex off;\n}' /etc/nginx/sites-available/default
 service nginx restart
