e0261c631f210969d58bc381dea85321b3169602
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-13 15:18:07 -0500

    data folder fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
 
-apt-get update -y
-apt-get install nginx -y
-service nginx restart
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo service nginx restart
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu data
-sed -i "/server_name _/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
-service nginx restart
+chown -R ubuntu:ubuntu /data/
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
+sudo sed -i "/server_name _/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart
