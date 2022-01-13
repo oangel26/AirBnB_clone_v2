f435b847ac34280782d473ca0614572d6826801e
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-13 16:58:20 -0500

    file name fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,12 +6,13 @@ of the web_static folder. Use the function do_pack.
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
-	local("mkdir -p /versions/")
-	day =datetime.now().strftime("%Y%m%d%H%m%s")
-	compr_file =  "/versions/web_static_{}".format(day)
-	local("tar -czf {}.tgz /web_static".format(compr_file))
-	try:
-		return compr_file
-	except: 
-		return None
+    try:
+        local("mkdir -p /versions/")
+        d_t = datetime.now().strftime("%Y%m%d%H%M%S")
+        compr_file = "versions/web_static_{}.tgz".format(d_t)
+        local("tar -cvzf {} web_static".format(compr_file))
+        return compr_file
+    except:
+        return None
