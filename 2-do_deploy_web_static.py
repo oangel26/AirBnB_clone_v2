f3889c065460846999744f4ad5bf82695589a4a5
jcgonzalezb <jcgonzalez@gmail.com>
2022-01-14 12:52:36 -0500

    syntax fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ import os
 from datetime import datetime
 
 env.hosts = ['34.139.188.96', '54.209.148.200']
+env.user = "ubuntu"
 
 
 def do_pack():
@@ -31,17 +32,17 @@ def do_deploy(archive_path):
     name_dir = archive_path[9:-4]
     if os.path.exists(archive_path):
         put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}/".format(name_dir))
-        run("tar -xzf /tmp/web_static_{}.tgz -C \
-        /data/web_static/releases/web_static_{}/".format(name_dir, name_dir))
-        run("rm /tmp/{}.tgz".format(name_dir))
-        run("mv /data/web_static/releases/web_static_{}/web_static/* \
-            /data/web_static/releases/web_static_{}/".
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(name_dir))
+        run("sudo tar -xzf /tmp/{}.tgz -C \
+        /data/web_static/releases/{}/".format(name_dir, name_dir))
+        run("sudo rm /tmp/{}.tgz".format(name_dir))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".
             format(name_dir, name_dir))
-        run("rm -rf /data/web_static/releases/web_static_{}/web_static"
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
             .format(name_dir))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/web_static_{}/ \
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ \
             /data/web_static/current".format(name_dir))
         print("New version deployed!")
         return True
